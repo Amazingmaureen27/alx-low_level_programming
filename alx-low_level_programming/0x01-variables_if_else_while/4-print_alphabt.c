a1a5779667468fcf400d525c226927eb41b559cf
Amazingmaureen27 <maureen.oluoha@gmail.com>
2023-10-09 15:58:35 -0700

    hot task

--- alx-low_level_programming/0x01-variables_if_else_while/4-print_alphabt.c
+++ alx-low_level_programming/0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: 0
-*
-*/
-
+ * main - Entry point
+ *
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+			continue;
 		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
